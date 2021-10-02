f72098aad84c4595e76a57b990f187d34d3cde21
Shiv Bindra <shivbindra94@gmail.com>
2021-10-01 20:21:09 -0400

    5 Web pages setup and node_modules added inside the gitignore file

---
+++ .history/routes/index_20211001155846.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home'});
+});
+
+/* GET home page back up if user writes /home. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home'});
+});
+
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About', page: 'about'});
+});
+
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects', page: 'projects'});
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services', page: 'services'});
+});
+
+/* GET Contact Me page. */
+router.get('/contact-me', function(req, res, next) {
+  res.render('index', { title: 'Contact-me', page: 'contact'});
+});
+
+
+module.exports = router;
